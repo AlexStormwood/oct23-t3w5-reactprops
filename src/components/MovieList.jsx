b9722f03051505695516f2fa7d8df44f25751d53
Alex H <alex.holder@bigfootds.com>
2024-05-02 19:57:33 +1000

    fix: validate on empty movie names

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -8,15 +8,18 @@ let favouriteMovies = [
 	"Aliens",
 	"Dune",
 	"Hot Fuzz",
+	"",
 	"Anchorman",
 	"Lock Stock and 2 Smoking Barrels",
 	"Shrek",
 	"Pokemon",
+	"",
 	"Blazing Saddles",
 	"Snatch",
 	"Shrek 2",
 	"The good, the bad, and the ugly",
 	"Robocop",
+	"",
 	"Bladerunner",
 	"Lord of the Rings 1-3"
 ];
@@ -26,6 +29,19 @@ export default function MovieList(){
 	return (
 	<section>
 		{favouriteMovies.map((movie, index) => {
+
+			// if (movie && movie.length === 0) {
+
+			// }
+
+			// if (movie.length !== 0 ){
+			// 	return <MovieCard key={movie + "-" + index} elephant={movie} />;
+			// }
+
+			if (movie.length === 0){
+				return null;
+			}
+
 			return <MovieCard key={movie + "-" + index} elephant={movie} />
 		})}
 	</section>
